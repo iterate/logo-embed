0980de6d2dd73e8f6abb176d62ba7720930e85ad
oddmunds <oddmunds@iterate.no>
2018-12-14 14:52:48 +0100

    bench

--- lib/rainbow/worker.ex
+++ lib/rainbow/worker.ex
@@ -3,43 +3,17 @@ defmodule Rainbow.Worker do
 
   alias Blinkchain.Point
 
-  defmodule State do
-    defstruct [:timer, :colors]
-  end
-
   def start_link(opts \\ []) do
     GenServer.start_link(__MODULE__, opts, name: __MODULE__)
   end
 
   def init(_opts) do
-    {:ok, ref} = :timer.send_interval(1000, :draw_frame)
-
-    state = %State{
-      timer: ref,
-      colors: Rainbow.colors()
-    }
-
-    {:ok, state}
+    {:ok, nil}
   end
 
-  def handle_info(:draw_frame, state) do
-    [c1, c2, c3, c4, c5, c6, c7, c8] = Enum.slice(state.colors, 0..7)
-    tail = Enum.slice(state.colors, 1..-1)
-
-    # Shift all pixels to the right
-    Blinkchain.copy(%Point{x: 0, y: 0}, %Point{x: 1, y: 0}, 7, 8)
-
-    # Populate the five leftmost pixels with new colors
-    Blinkchain.set_pixel(%Point{x: 0, y: 0}, c1)
-    Blinkchain.set_pixel(%Point{x: 0, y: 1}, c2)
-    Blinkchain.set_pixel(%Point{x: 0, y: 2}, c3)
-    Blinkchain.set_pixel(%Point{x: 0, y: 3}, c4)
-    Blinkchain.set_pixel(%Point{x: 0, y: 4}, c5)
-    Blinkchain.set_pixel(%Point{x: 0, y: 5}, c6)
-    Blinkchain.set_pixel(%Point{x: 0, y: 6}, c7)
-    Blinkchain.set_pixel(%Point{x: 0, y: 7}, c8)
-
+  def handle_cast({:bench, _intensity, color}, _state) do
+    Blinkchain.fill(%Point{x: 0, y: 0}, 8, 8, color)
     Blinkchain.render()
-    {:noreply, %State{state | colors: tail ++ [c1]}}
+    {:noreply, nil}
   end
 end
