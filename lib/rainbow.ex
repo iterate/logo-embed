2f151f75c27f8054b302419c6c7323ea946cbd32
Oddmund Strømme <oddmunds@iterate.no>
2019-06-19 16:59:23 +0200

    grrrrr

--- lib/rainbow.ex
+++ lib/rainbow.ex
@@ -2,14 +2,14 @@ defmodule Rainbow do
   alias Blinkchain.Color
 
   @colors [
-    Color.parse("#9400D3"),
-    Color.parse("#4B0082"),
-    Color.parse("#0000FF"),
-    Color.parse("#00FF00"),
-    Color.parse("#FFFF00"),
-    Color.parse("#FF7F00"),
+    Color.parse("#FFFFFF"),
     Color.parse("#FF0000"),
-    Color.parse("#000000")
+    Color.parse("#00FF00"),
+    Color.parse("#0000FF"),
+    Color.parse("#FFFFFF"),
+    Color.parse("#FF0000"),
+    Color.parse("#00FF00"),
+    Color.parse("#0000FF")
   ]
 
   def colors, do: @colors
--- lib/rainbow/worker.ex
+++ lib/rainbow/worker.ex
@@ -1,19 +1,46 @@
 defmodule Rainbow.Worker do
   use GenServer
-
   alias Blinkchain.Point
+
+  defmodule State do
+    defstruct [:timer, :colors]
+  end
 
   def start_link(opts \\ []) do
     GenServer.start_link(__MODULE__, opts, name: __MODULE__)
   end
 
   def init(_opts) do
-    {:ok, nil}
+    # Send ourselves a message to draw each frame every 33 ms,
+    # which will end up being approximately 15 fps.
+    {:ok, ref} = :timer.send_interval(100, :draw_frame)
+
+    state = %State{
+      timer: ref,
+      colors: Rainbow.colors()
+    }
+
+    {:ok, state}
   end
 
-  def handle_cast({:bench, _intensity, color}, _state) do
-    Blinkchain.fill(%Point{x: 0, y: 0}, 8, 8, color)
+  def handle_info(:draw_frame, state) do
+    [c1, c2, c3, c4, c5, c6, c7, c8] = Enum.slice(state.colors, 0..7)
+    tail = Enum.slice(state.colors, 1..-1)
+
+    # Shift all pixels to the right
+    Blinkchain.copy(%Point{x: 0, y: 0}, %Point{x: 1, y: 0}, 7, 8)
+
+    # Populate the five leftmost pixels with new colors
+    Blinkchain.set_pixel(%Point{x: 0, y: 0}, c1)
+    Blinkchain.set_pixel(%Point{x: 0, y: 1}, c2)
+    Blinkchain.set_pixel(%Point{x: 0, y: 2}, c3)
+    Blinkchain.set_pixel(%Point{x: 0, y: 3}, c4)
+    Blinkchain.set_pixel(%Point{x: 0, y: 4}, c5)
+    Blinkchain.set_pixel(%Point{x: 0, y: 5}, c6)
+    Blinkchain.set_pixel(%Point{x: 0, y: 6}, c7)
+    Blinkchain.set_pixel(%Point{x: 0, y: 7}, c8)
+
     Blinkchain.render()
-    {:noreply, nil}
+    {:noreply, %State{state | colors: tail ++ [c1]}}
   end
 end
